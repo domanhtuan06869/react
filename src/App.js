f71f7376a8714c5e5ced57b7bb974d45a470c2f3
Đỗ Mạnh Tuân <46439071+domanhtuan06869@users.noreply.github.com>
2019-11-19 14:17:22 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import axios from 'axios';
+ 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Homeđf</Link>
+        </li>
+        <li>
+          <Link to="/getlist">Aboutcdfdgdfđ</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+
+      <hr />
+      <div className="main-route-place">
+       
+      </div>
+    
     </div>
+    <Route exact path="/" component={Home} />
+       <Route exact path="/getlist" component={apache}></Route>
+  </BrowserRouter>
   );
 }
 
+function Home(){
+  const [list,setList]=useState('')
+ 
+  useEffect(()=>{
+
+    axios.get('/ping')
+      .then(res => {
+    console.log(res.data)
+    setList(res.data)
+      })
+      .catch(error => console.log(error));
+ 
+  },[])
+  return (
+    <div>
+<button onClick={()=>alert('hggj')}>sàasfsdg</button>
+<h1>{list}</h1>
+    </div>
+  );
+}
+function apache(){
+  return (
+    <div>
+      <h2>cdsfsdfsd</h2>
+    </div>
+  );
+}
 export default App;
